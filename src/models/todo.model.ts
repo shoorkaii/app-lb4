512ed4c5150c2afab3a5faffaa88340b70edba7b
reza <shoorkaii@gmail.com>
2020-08-17 22:25:40 +0430

    geo services added

---
+++ src/models/todo.model.ts
@@ -0,0 +1,47 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Todo extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: false,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  title: string;
+
+  @property({
+    type: 'string',
+  })
+  desc?: string;
+
+  @property({
+    type: 'boolean',
+  })
+  isComplete?: boolean;
+
+  @property({
+    type: 'string',
+  })
+  remindAtAddress?: string;
+
+  @property({
+    type: 'string',
+  })
+  remindAtGeo?: string;
+
+  constructor(data?: Partial<Todo>) {
+    super(data);
+  }
+}
+
+export interface TodoRelations {
+  // describe navigational properties here
+}
+
+export type TodoWithRelations = Todo & TodoRelations;
---
+++ src/services/geocoder.service.ts
@@ -0,0 +1,29 @@
+import {getService} from '@loopback/service-proxy';
+import {inject, Provider} from '@loopback/core';
+import {GeocoderDataSource} from '../datasources';
+
+export interface GeoPoit {
+    y: number,
+    x: number
+}
+
+export interface Geocoder {
+    // this is where you define the Node.js methods that will be
+    // mapped to REST/SOAP/gRPC operations as stated in the datasource
+    // json file.
+
+    geocode(address: string): Promise<GeoPoit[]>;
+}
+
+export class GeocoderProvider implements Provider<Geocoder> {
+    constructor(
+        // geocoder must match the name property in the datasource json file
+        @inject('datasources.geocoder')
+        protected dataSource: GeocoderDataSource = new GeocoderDataSource(),
+    ) {
+    }
+
+    value(): Promise<Geocoder> {
+        return getService(this.dataSource);
+    }
+}
