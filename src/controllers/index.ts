e92afa24872f05f93552491f8dacb089707e23d2
reza <shoorkaii@gmail.com>
2020-08-18 10:15:39 +0430

    add postgresql datasource

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -3,3 +3,4 @@ export * from './hello.controller';
 export * from './todo.controller';
 export * from './todo-list-todo.controller';
 export * from './todo-todo-list.controller';
+export * from './todo-list.controller';
--- src/datasources/db.datasource.ts
+++ src/datasources/db.datasource.ts
@@ -3,9 +3,13 @@ import {juggler} from '@loopback/repository';
 
 const config = {
   name: 'db',
-  connector: 'memory',
-  localStorage: '',
-  file: './data/db.json'
+  connector: 'postgresql',
+  url: '',
+  host: '',
+  port: 0,
+  user: '',
+  password: '',
+  database: ''
 };
 
 // Observe application's life cycle to disconnect the datasource when
